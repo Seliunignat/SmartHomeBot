e99433e8d76be52f2f7491a1ead53314c9b3aba8
Ignat Seliun <seliunignat@gmail.com>
2021-07-04 18:29:42 +0300

    Created version of SmartHomeBot with blynk, ewelink, also connected MongoDB

---
+++ src/main/java/Main.java
@@ -0,0 +1,104 @@
+import com.github.realzimboguy.ewelink.api.EweLink;
+import com.github.realzimboguy.ewelink.api.model.devices.DeviceItem;
+import com.github.realzimboguy.ewelink.api.model.devices.Devices;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.*;
+
+public class Main {
+
+    public static Map<String, String> ENV;
+    private static Map<String, String> COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES;
+    private static Map<String, Map.Entry<String, String>> COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES;
+    private static String blynkAuthToken;
+    private static List<Long> listOfAdmins;
+    private static EweLink eweLink;
+    private static Devices devices;
+    private static final String DEVICE_STATUS_ON = "on";
+    private static final String DEVICE_STATUS_OFF = "off";
+
+    public static void main(String[] args) throws TelegramApiException {
+        ENV = System.getenv();
+        loadData();
+        connectMongo();
+        try {
+            loadEwelink();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(new SmartHomeBot(ENV, COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES, listOfAdmins, eweLink, COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void loadData(){
+        blynkAuthToken = ENV.get("BLYNK_AUTH_TOKEN");
+        listOfAdmins = new ArrayList<>();
+        listOfAdmins.add(Long.parseLong(ENV.get("ADMIN_ID")));
+        COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES = new HashMap<>();
+        COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.put("Включить настольную лампу", "/update/V3?value=1");
+        COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.put("Выключить настольную лампу", "/update/V3?value=0");
+        COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.put("Включить цветную лампу", "/update/V2?value=1");
+        COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.put("Выключить цветную лампу", "/update/V2?value=0");
+        COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.put("Статус настольной лампы", "/get/V3");
+        COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.put("Статус цветной лампы", "/get/V2");
+        COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES = new HashMap<>();
+        COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.put("Включить свет на терассе",
+                new AbstractMap.SimpleEntry<String, String>(ENV.get("DEVICE_TERRACE_ID"), DEVICE_STATUS_ON) {
+                });
+        COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.put("Выключить свет на терассе",
+                new AbstractMap.SimpleEntry<>(ENV.get("DEVICE_TERRACE_ID"), DEVICE_STATUS_OFF));
+        COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.put("Включить свет на крыльце",
+                new AbstractMap.SimpleEntry<>(ENV.get("DEVICE_PORCH_ID"), DEVICE_STATUS_ON));
+        COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.put("Выключить свет на крыльце",
+                new AbstractMap.SimpleEntry<>(ENV.get("DEVICE_PORCH_ID"), DEVICE_STATUS_OFF));
+        COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.put("Включить свет под навесом",
+                new AbstractMap.SimpleEntry<>(ENV.get("DEVICE_CANOPY_ID"), DEVICE_STATUS_ON));
+        COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.put("Выключить свет под навесом",
+                new AbstractMap.SimpleEntry<>(ENV.get("DEVICE_CANOPY_ID"), DEVICE_STATUS_OFF));
+
+    }
+
+    public static void connectMongo(){
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://" + ENV.get("MONGO_USER") + ":" + ENV.get("MONGO_PASSWORD") + "@smarthomebotdb.iqsez.mongodb.net/SmartHomeBotDB?retryWrites=true&w=majority");
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+
+        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoCollection<Document> toys = database.getCollection("toys");
+//        Document toy = new Document("name", "yoyo") .append("ages", new Document("min", 5));
+//        ObjectId id = toys.insertOne(toy).getInsertedId().asObjectId().getValue();
+//
+//        Document yoyo = toys.find(new Document("name", "yoyo")).first();
+//        System.out.println(yoyo.toJson());
+    }
+
+    private static void loadEwelink() throws Exception {
+        eweLink = new EweLink("eu", ENV.get("EWELINK_EMAIL"), ENV.get("EWELINK_PASSWORD"), 60);
+        eweLink.login();
+        devices = eweLink.getDevices();
+        for (DeviceItem devicelist : devices.getDevicelist()) {
+            System.out.println(devicelist.getDeviceid());
+            System.out.println(devicelist.getName());
+
+            eweLink.getDeviceStatus(devicelist.getDeviceid());
+            System.out.println(eweLink.getDeviceStatus(devicelist.getDeviceid()));
+        }
+    }
+}
---
+++ src/main/java/SmartHomeBot.java
@@ -0,0 +1,132 @@
+import com.github.realzimboguy.ewelink.api.EweLink;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class SmartHomeBot extends TelegramLongPollingBot {
+
+    private static Map<String, String> ENV;
+    private Map<String, String> COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES;
+    private Map<String, Map.Entry<String, String>> COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES;
+    private List<Long> listOfAdmins;
+    private EweLink ewelink;
+
+    public SmartHomeBot(Map<String, String> ENV, Map<String, String> commands_and_requests_for_blynk_devices, List<Long> listOfAdmins, EweLink eweLink, Map<String, Map.Entry<String, String>> commands_id_status_for_ewelink_devices){
+        this.ENV = ENV;
+        this.COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES = commands_and_requests_for_blynk_devices;
+        this.listOfAdmins = listOfAdmins;
+        this.ewelink = eweLink;
+        this.COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES = commands_id_status_for_ewelink_devices;
+    }
+
+    public String getBotUsername() {
+        return ENV.get("BOT_USERNAME");
+    }
+
+    public String getBotToken() {
+        return ENV.get("BOT_TOKEN");
+    }
+
+    public void onUpdateReceived(Update update) {
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy hh:mm a");
+        Message message = update.getMessage();
+        if(message != null && message.hasText()){
+            try {
+                System.out.print(formatForDateNow.format(dateNow) + " ");
+                System.out.println(message.getFrom().getFirstName() + " " + message.getFrom().getLastName()
+                        + " (@" + message.getFrom().getUserName() + ", id: "
+                        + message.getFrom().getId() + ")" + ": " + message.getText());
+
+                if(COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.get(message.getText()) != null && listOfAdmins.contains(message.getFrom().getId())) {
+                    Client client = ClientBuilder.newClient();
+                    Response response = client.target("http://blynk-cloud.com/" + ENV.get("BLYNK_AUTH_TOKEN") + COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.get(message.getText()))
+                            .request(MediaType.TEXT_PLAIN_TYPE)
+                            .get();
+
+                    System.out.println("status: " + response.getStatus());
+                    System.out.println("headers: " + response.getHeaders());
+                    System.out.println("body:" + response.readEntity(String.class));
+                }
+                else if(COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.get(message.getText()) != null && listOfAdmins.contains(message.getFrom().getId())){
+                    try {
+                        ewelink.setDeviceStatus(COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.get(message.getText()).getKey(), COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.get(message.getText()).getValue());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                SendMessage sendMessage = new SendMessage(Long.toString(message.getChatId()), "Вы написали: " + message.getText());
+                setButtons(sendMessage);
+                execute(sendMessage);
+
+                switch (message.getText()){
+//                    case "Статус гирлянды":
+//                    {
+//                        Client client = ClientBuilder.newClient();
+//                        Response response = client.target("http://blynk-cloud.com/" + blynkAuthToken + COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.get(message.getText()))
+//                                .request(MediaType.TEXT_PLAIN_TYPE)
+//                                .get();
+//
+//                        System.out.println("status: " + response.getStatus());
+//                        System.out.println("headers: " + response.getHeaders());
+//                        System.out.println("body:" + response.readEntity(String.class));
+//                        break;
+//                    }
+//                    case "Включить гирлянду":
+//                    {
+//                        Client client = ClientBuilder.newClient();
+//                        Response response = client.target("http://blynk-cloud.com/" + blynkAuthToken + COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.get(message.getText()))
+//                                .request(MediaType.TEXT_PLAIN_TYPE)
+//                                .get();
+//
+//                        System.out.println("status: " + response.getStatus());
+//                        System.out.println("headers: " + response.getHeaders());
+//                        System.out.println("body:" + response.readEntity(String.class));
+//                        break;
+//                    }
+                    default: break;
+                }
+
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setButtons(SendMessage sendMessage){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<KeyboardRow>();
+        for (String command :
+                COMMANDS_AND_REQUESTS_FOR_BLYNK_DEVICES.keySet()) {
+            KeyboardRow keyboardRow = new KeyboardRow();
+            keyboardRow.add(new KeyboardButton(command));
+            keyboardRowList.add(keyboardRow);
+        }
+        for (String command :
+                COMMANDS_ID_STATUS_FOR_EWELINK_DEVICES.keySet()) {
+            KeyboardRow keyboardRow = new KeyboardRow();
+            keyboardRow.add(new KeyboardButton(command));
+            keyboardRowList.add(keyboardRow);
+        }
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    }
+}
